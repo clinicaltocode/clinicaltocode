d9f63e90a4f47e06980012e324bdd287da68ad2e
clinicaltocode <clinicaltocode@gmail.com>
2025-08-26 08:48:43 -0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Clinical to Code - Where Healthcare Meets Technology',
+  description: 'Bridging clinical expertise with healthcare IT',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
