4264e78442dea02ebd2f70e5a4884bdad4cd8c93
clinicaltocode <clinicaltocode@gmail.com>
2025-08-26 12:41:36 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,14 @@
-export const metadata = { title: 'Clinical to Code' }
+export const metadata = {
+  title: 'Clinical to Code',
+  description: 'Where Healthcare Meets Technology',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
     </html>
   )
 }
